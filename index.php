451f7284dddc59435ac9d60cc63eee2e642d734e
root <KollanH@gmail.com>
2015-09-27 18:36:19 -0500

    added core features to home/ .

--- index.php
+++ index.php
@@ -1,27 +1,92 @@
 <html>
-<head>
-<title>rebecca cordalis-selvig | editorial stylist</title>
-<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-</head>
-<body>
-<header>
-Work In Progress
-</header>
-<nav>
-</nav>
-<section>
-</section>
-<footer>
-</footer>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	<head>
+		<title>rebecca cordalis-selvig | editorial stylist</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	</head>
+	<body>
+		<div id="wrap">
+			<header id="header" role="banner">
+				<div class="inner container">
+					<div class="info">
+						<ul class="social-links">
+							<li><a class="fa fa-facebook facebook-btn" href="https://www.facebook.com/rcselvig" target="_blank"></a></li>
+							<li><a class="fa fa-twitter twitter-btn" href="https://twitter.com/rcselvig" target="_blank"></a></li>
+							<li><a class="fa fa-instagram instagram-btn" href="https://instagram.com/rcselvig" target="_blank"></a></li>
+							<li><a class="fa fa-tumblr tumblr-btn" href="http://rcselvig.tumblr.com/" target="_blank"></a></li>
+							<li><a class="fa fa-pinterest pinterest-btn" href="https://www.pinterest.com/rcselvig/" target="_blank"></a></li>
+							<li><a class="fa fa-youtube youtube-btn" href="" target="_blank"></a></li>
+							<li><a class="fa fa-linkedin linkedin-btn" href="https://www.linkedin.com/in/rpcsel" target="_blank"></a></li>
+						</ul>
+					</div>
+					<nav class="main-navigation navigation">
+						<ul id="menu-main" class="clearfix menu">
+							<li class="menu-item"><a href="#">About</a></li>
+							<li class="menu-item"><a href="#">Work</a></li>
+							<li class="menu-item"><a href="#">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</header>
+			<div id="main" class="site-main" role="main">
+				<div id="single">
+					<div class="content">
+						<div class="row">
+							<div class="container">
+								<div class="inner">
+									<h2 class="title">Rebecca Cordalis-Selvig</h2>
+									<img src="assets/images/home-image.jpg" alt="rebecca cordalis-selvig" />
+									<div class="col-md-4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu libero, euismod in eros sed, interdum tempor neque. Phasellus rutrum, purus sit amet viverra rutrum, purus est auctor magna, non porttitor urna metus at ex. Ut elementum auctor porta. Etiam consectetur mauris nisi, a rhoncus dui mollis non. Vestibulum venenatis sagittis sollicitudin. Suspendisse ut tellus metus. Proin tempor mauris libero. Fusce quis convallis nisl. Proin pharetra risus sed vulputate egestas. Morbi tortor eros, blandit ut ullamcorper vitae, commodo at velit. Nulla orci mi, imperdiet nec venenatis vitae, dapibus ac leo. Aenean a augue enim. Interdum et malesuada fames ac ante ipsum primis in faucibus.
 
-  ga('create', 'UA-68144791-1', 'auto');
-  ga('send', 'pageview');
+Nullam eu gravida dui, et feugiat urna. Pellentesque vel lobortis dui. Proin facilisis tempor nisi, vel semper urna bibendum et. Donec nec lectus rhoncus, pulvinar tortor nec, rutrum turpis. Curabitur rhoncus malesuada ullamcorper. Ut ut nulla a est finibus sodales. Etiam eget sodales lorem, eu lobortis ligula. Sed porta ex vitae ornare laoreet. Aliquam accumsan eu erat sit amet venenatis. Vestibulum sed erat ac lorem suscipit congue. Suspendisse fringilla convallis erat, quis convallis nulla consequat a. Maecenas viverra ante magna, eu viverra magna ultricies in. Duis et facilisis tortor.</p></div>
+									<div class="col-md-4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu libero, euismod in eros sed, interdum tempor neque. Phasellus rutrum, purus sit amet viverra rutrum, purus est auctor magna, non porttitor urna metus at ex. Ut elementum auctor porta. Etiam consectetur mauris nisi, a rhoncus dui mollis non. Vestibulum venenatis sagittis sollicitudin. Suspendisse ut tellus metus. Proin tempor mauris libero. Fusce quis convallis nisl. Proin pharetra risus sed vulputate egestas. Morbi tortor eros, blandit ut ullamcorper vitae, commodo at velit. Nulla orci mi, imperdiet nec venenatis vitae, dapibus ac leo. Aenean a augue enim. Interdum et malesuada fames ac ante ipsum primis in faucibus.
 
-</script>
-</body>
+Nullam eu gravida dui, et feugiat urna. Pellentesque vel lobortis dui. Proin facilisis tempor nisi, vel semper urna bibendum et. Donec nec lectus rhoncus, pulvinar tortor nec, rutrum turpis. Curabitur rhoncus malesuada ullamcorper. Ut ut nulla a est finibus sodales. Etiam eget sodales lorem, eu lobortis ligula. Sed porta ex vitae ornare laoreet. Aliquam accumsan eu erat sit amet venenatis. Vestibulum sed erat ac lorem suscipit congue. Suspendisse fringilla convallis erat, quis convallis nulla consequat a. Maecenas viverra ante magna, eu viverra magna ultricies in. Duis et facilisis tortor.</p></div>
+									<div class="col-md-4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu libero, euismod in eros sed, interdum tempor neque. Phasellus rutrum, purus sit amet viverra rutrum, purus est auctor magna, non porttitor urna metus at ex. Ut elementum auctor porta. Etiam consectetur mauris nisi, a rhoncus dui mollis non. Vestibulum venenatis sagittis sollicitudin. Suspendisse ut tellus metus. Proin tempor mauris libero. Fusce quis convallis nisl. Proin pharetra risus sed vulputate egestas. Morbi tortor eros, blandit ut ullamcorper vitae, commodo at velit. Nulla orci mi, imperdiet nec venenatis vitae, dapibus ac leo. Aenean a augue enim. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+
+Nullam eu gravida dui, et feugiat urna. Pellentesque vel lobortis dui. Proin facilisis tempor nisi, vel semper urna bibendum et. Donec nec lectus rhoncus, pulvinar tortor nec, rutrum turpis. Curabitur rhoncus malesuada ullamcorper. Ut ut nulla a est finibus sodales. Etiam eget sodales lorem, eu lobortis ligula. Sed porta ex vitae ornare laoreet. Aliquam accumsan eu erat sit amet venenatis. Vestibulum sed erat ac lorem suscipit congue. Suspendisse fringilla convallis erat, quis convallis nulla consequat a. Maecenas viverra ante magna, eu viverra magna ultricies in. Duis et facilisis tortor.</p></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="instagram">
+				<header class="header">
+					<h2 class="title">Instagram</h2>
+					<a class="white-btn" href="http://instagram.com/rcselvig"><i class="fa fa-instagram icon-instagram"></i> @rcselvig</a>
+				</header>
+				<div id="instafeed" class="images clearfix"></div>
+			</div>
+			<footer>
+			</footer>
+		</div>
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/js/instafeed.min.js"></script>
+		<script type="text/javascript">
+	    var feed = new Instafeed({
+	        get: 'user',
+	        accessToken: '674909336.e4fca26.7906b0e36f3e41f58a6b17ee12e179b6',
+	        userId: 674909336,
+	        sortBy: 'most-recent',
+	        links: true,
+	        limit: 6,
+	        resolution: 'standard_resolution',
+	        clientId: 'e4fca26f6ec747c185a98e84db533f49',
+	        template: '<a class="image overlay-btn" href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
+	    });
+	    feed.run();
+		</script>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-68144791-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+	</body>
 </html>
