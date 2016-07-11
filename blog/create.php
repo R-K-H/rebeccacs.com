0ca31dad74e91bfda890a464b2dfe08386e92105
Kollan <KollanH@gmail.com>
2016-07-10 22:50:52 -0500

    added in images, fixed some urls and tested a few blog post creations.

--- blog/create.php
+++ blog/create.php
@@ -3,6 +3,10 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 $basedir = dirname(__FILE__); 
 $content = $_POST['page'];
-$myfile = fopen($basedir . DIRECTORY_SEPARATOR . 'blog_test.html', 'w+') or die("Unable to open file!");
+$title = $_POST['title'];
+$cleanTitle = urlencode ($title);
+$myfile = fopen($basedir . DIRECTORY_SEPARATOR . $title.'.html', 'w+') or die("Unable to open file!");
 fwrite($myfile, $content);
 fclose($myfile);
+sleep(1);
+header('Location: http://rebeccacs.com/blog/'.$title.'.html'); /* Redirect browser */
--- blog/post.php
+++ blog/post.php
@@ -1,12 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<form action="create.php" method="post">
-	<textarea name="page" placeholder="paste page content here..."></textarea>
-	<button type="submit">Create Post</button>
-</form>
-</body>
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>Create HTML Page</title>
+        <link rel="stylesheet" href="../assets/css/variant-icons.css">
+        <link rel="stylesheet" href="../assets/css/variant-social-icons.css">
+        <link rel="stylesheet" href="../assets/css/normalize.css">
+        <link rel="stylesheet" href="../assets/css/style.css">
+        <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet" type="text/css">
+                
+        
+        <link href="../assets/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+        <link href="../assets/css/themify-icons.css" rel="stylesheet" type="text/css" media="all">
+        <link href="../assets/css/flexslider.css" rel="stylesheet" type="text/css" media="all">
+        <link href="../assets/css/lightbox.min.css" rel="stylesheet" type="text/css" media="all">
+        <link href="../assets/css/ytplayer.css" rel="stylesheet" type="text/css" media="all">
+        <link href="../assets/css/theme.css" rel="stylesheet" type="text/css" media="all">
+        <link href="../assets/css/custom.css" rel="stylesheet" type="text/css" media="all">
+        <link href="http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <h1>Create A Page</h1>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <form action="create.php" method="post">
+                    <input type="text" name="title" placeholder="post title.." />
+                    <textarea name="page" placeholder="paste page content here..." rows="25"></textarea>
+                    <button type="submit">Create Post</button>
+                </form>
+            </div>
+        </div>
+    </body>
 </html>
