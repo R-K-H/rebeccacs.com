c0cf9a19c654803c50e2f03b9b12784f164dcccf
CTR <ctr@Larss-MacBook-Pro.local>
2016-08-02 07:11:24 -1000

    added missing username. added in composer.

--- index.php
+++ index.php
@@ -162,7 +162,7 @@
 			<div id="instagram">
 				<header class="header">
 					<h2 class="title">Instagram</h2>
-					<a class="white-btn" href="http://instagram.com/rcselvig" target="_blank" onclick="ga('send', 'event', 'external click', 'click', 'instagram');"><i class="fa fa-instagram icon-instagram"></i> @rcselvig</a>
+					<a class="white-btn" href="http://instagram.com/rebecca__cs" target="_blank" onclick="ga('send', 'event', 'external click', 'click', 'instagram');"><i class="fa fa-instagram icon-instagram"></i> @rebecca__cs</a>
 				</header>
 				<div id="instafeed" class="images clearfix"></div>
 			</div>
