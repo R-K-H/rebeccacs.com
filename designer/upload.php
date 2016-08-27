15a908f56936b947abde12f7a753ff0e39874dfb
Kollan House <kollanh@gmail.com>
2016-08-27 15:48:41 -0700

    fixed file location.

--- designer/upload.php
+++ designer/upload.php
@@ -1,13 +1,13 @@
 <?php
 $ds          = DIRECTORY_SEPARATOR;  //1
- 
+$path = $_SERVER['DOCUMENT_ROOT'];
 $storeFolder = 'uploads';   //2
  
 if (!empty($_FILES)) {
      
     $tempFile = $_FILES['file']['tmp_name'];          //3             
       
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
+    $targetPath = $path . $ds. 'assets/images' . $ds . $storeFolder . $ds;  //4
      
     $targetFile =  $targetPath. $_FILES['file']['name'];  //5
  
