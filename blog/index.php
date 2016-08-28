894366a088a07245568f2f271956a8a8f1f2c7b0
Kollan House <kollanh@gmail.com>
2016-08-28 15:27:25 -0700

    added for new create.

--- blog/index.php
+++ blog/index.php
@@ -3,17 +3,16 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . '/layouts/header.php';
 ?>
 <div class="no-gutter row"></div>
-			<div class="container">
-				<div class="your-class">
-					<?php // Gets our files.
-						$directory = $_SERVER['DOCUMENT_ROOT'] . '/blog';
-						$scanned_directory = array_diff(scandir($directory), array('..', '.', 'index.php'));
-						$files = array();
-						foreach($scanned_directory as $file) { 
-							echo '<a href="'.$file.'">'.$file.'</a>'; 
-						}
-					?>
-			</div>
+	<div class="container">
+			<?php // Gets our files.
+				$directory = $_SERVER['DOCUMENT_ROOT'] . '/blog';
+				$scanned_directory = array_diff(scandir($directory), array('..', '.', 'index.php'));
+				$files = array();
+				foreach($scanned_directory as $file) { 
+					echo '<a href="'.$file.'">'.$file.'</a>'; 
+				}
+			?>
+	</div>
 <?php 
     require_once $_SERVER['DOCUMENT_ROOT'] . '/layouts/foot.php';
 ?>
