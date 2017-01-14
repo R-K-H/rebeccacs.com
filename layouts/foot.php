66b1b2a75e55726eaca018011ce83d4cea36a469
Kollan House <kollanh@gmail.com>
2017-01-14 02:42:35 +0000

    split out css. added beter description. adjusted formatting.

--- layouts/foot.php
+++ layouts/foot.php
@@ -2,7 +2,7 @@
 		</div>
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
-		<script src="//cdn.rawgit.com/noelboss/featherlight/1.3.5/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
+		<script type="text/javascript" src="//cdn.rawgit.com/noelboss/featherlight/1.3.5/release/featherlight.min.js" charset="utf-8"></script>
 		<script type="text/javascript" src="/assets/js/jquery.lazyload.js"></script>
 		<script type="text/javascript" src="/assets/js/submitcontact.js"></script>
 		<script type="text/javascript" src="/assets/js/instafeed.min.js"></script>
